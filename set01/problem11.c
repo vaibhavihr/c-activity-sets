af8b53e07a12bb2afd4549ec9271b29f1de5e302
vaibhavihr <vaibhavihr.bsc23@rvu.edu.in>
2024-01-24 21:50:00 +0530

    test

--- set01/problem11.c
+++ set01/problem11.c
@@ -11,5 +11,33 @@ Complex add_complex(Complex a, Complex b);
 void output(Complex a, Complex b, Complex sum);
 int main()
 {
-
+	Complex a,b,sum;
+	printf("Enter the first complex number:\n");
+	a=input_complex();
+	printf("Enter the second complex number:\n");
+	b=input_complex();
+	sum=add_complex(a,b);
+	output(a,b,sum);
+	return 0;
+}
+Complex input_complex()
+{
+	Complex n;
+	printf("Enter the real part:");
+	scanf("%f",&n.real);
+	printf("Enter the imaginary part:");
+	scanf("%f",&n.imaginary);
+	return n;
+}
+Complex add_complex(Complex a, Complex b)
+{
+	Complex sum;
+	sum.real=a.real+b.real;
+	sum.imaginary=a.imaginary+a.imaginary;
+	return sum;
+}
+void output(Complex a, Complex b, Complex sum)
+{
+	printf("Sum of complex numbers:\n");
+	printf("(%f+%fi)+(%f+%fi)=%f +%fi\n",a.real,a.imaginary,b.real,b.imaginary,sum.real,sum.imaginary);
 }
