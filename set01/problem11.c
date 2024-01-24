c922e698466c6b3c580d8dad7cfa1fba23c6e6a4
vaibhavihr <vaibhavihr.bsc23@rvu.edu.in>
2024-01-24 21:26:42 +0530

    test

---
+++ set01/problem11.c
@@ -0,0 +1,15 @@
+//11. Write a C program to find the sum of 2 complex numbers
+#include<stdio.h>
+struct _complex {
+	float real;
+	float imaginary;
+};
+typedef struct _complex Complex;
+
+Complex input_complex();
+Complex add_complex(Complex a, Complex b);
+void output(Complex a, Complex b, Complex sum);
+int main()
+{
+
+}
