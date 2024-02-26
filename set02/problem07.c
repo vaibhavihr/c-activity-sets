22348f1666a15d024f818b0a0feff0cae2518a6c
vaibhavihr <vaibhavihr.bsc23@rvu.edu.in>
2024-02-26 14:15:05 +0530

    test

--- set02/problem07.c
+++ set02/problem07.c
@@ -10,5 +10,26 @@ void find_area(Triangle *t);
 void output(Triangle t);
 
 int main(){
-    
+	Triangle t;
+	t=input_triangle();
+	find_area(&t);
+	output(t);
+	return 0;
+}
+Triangle input_triangle()
+{
+	Triangle t;
+	printf("Enter the base:");
+	scanf("%f",&t.base);
+	printf("Enter the altitude:");
+	scanf("%f",&t.altitude);
+	return t;
+}
+void find_area(Triangle *t)
+{
+	t->area = 0.5 * (t->base*t->altitude);
+}
+void output(Triangle t)
+{
+	printf("The area of traingle with %f base and %f altitude is %f",t.base,t.altitude,t.area);
 }
---
+++ set03/problem02.c
@@ -0,0 +1,5 @@
+//2. Write a program to find whether the given 3 points form a triangle
+#include<stdio.h>
+void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float *y3);
+int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3);
+void output(float x1, float y1, float x2, float y2,float x3, float y3, int result);
