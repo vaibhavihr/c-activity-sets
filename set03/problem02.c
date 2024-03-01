edaf821d36ed0fb9420baa71818f0f6c1ac3967c
vaibhavihr <vaibhavihr.bsc23@rvu.edu.in>
2024-03-01 13:53:16 +0530

    committed 3/1/2024, 1:53:16 PM

--- set03/problem02.c
+++ set03/problem02.c
@@ -27,7 +27,7 @@ void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float
 
 int is_triangle(float x1, float y1, float x2, float y2, float x3, float y3) {
 
-    if ((x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) != 0)
+    if (((x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2))/2)!= 0)
         return 1; 
     else
         return 0; 
